898febe00a01865e61c988e2f679d749fad9eab7
Samuel Marlhens <samuel.marlhens@modis.com>
2019-03-15 11:49:14 +0100

    Added Firebase config to env

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import * as firebase from 'firebase';
+import {environment} from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,8 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   title = 'maSuperAgence';
+
+  constructor() {
+    firebase.initializeApp(environment.firebase);
+  }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: null,
+    authDomain: null,
+    databaseURL: null,
+    projectId: null,
+    storageBucket: null,
+    messagingSenderId: null
+  }
 };
