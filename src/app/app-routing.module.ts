d3973de099e920bd6d03afa6fca3321fed63ec8f
Samuel Marlhens <samuel.marlhens@modis.com>
2019-03-15 11:55:42 +0100

    Added admin login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {AdminSigninComponent} from './admin/admin-signin/admin-signin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'admin/login', component: AdminSigninComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { AdminSigninComponent } from './admin/admin-signin/admin-signin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AdminSigninComponent
   ],
   imports: [
     BrowserModule,
