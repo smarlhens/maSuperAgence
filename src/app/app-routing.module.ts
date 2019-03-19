d44ac8401dcfb72109e2f642a683bca13902c50d
Samuel Marlhens <samuel.marlhens@modis.com>
2019-03-19 10:01:32 +0100

    Step 22 : added AuthGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import {AdminSigninComponent} from './admin/admin-signin/admin-signin.component'
 import {AdminDashboardComponent} from './admin/admin-dashboard/admin-dashboard.component';
 import {HomeComponent} from './home/home.component';
 import {SinglePropertyComponent} from './single-property/single-property.component';
+import {AuthGuardService} from './services/auth-guard.service';
 
 const routes: Routes = [
-  {path: 'admin/login', component: AdminSigninComponent},
-  {path: 'admin/dashboard', component: AdminDashboardComponent},
   {path: 'home', component: HomeComponent},
-  {path: 'property/:id', component: SinglePropertyComponent}
+  {path: 'property/:id', component: SinglePropertyComponent},
+  {path: 'admin/login', component: AdminSigninComponent},
+  {path: 'admin/dashboard', canActivate: [AuthGuardService], component: AdminDashboardComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: '**', redirectTo: 'home'}
 ];
 
 @NgModule({
